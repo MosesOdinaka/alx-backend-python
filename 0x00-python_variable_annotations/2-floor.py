61183ec9fc317b5c5b025d87c42d8215712fd134
root <root@DESKTOP-SK90JMN>
2024-03-14 22:29:54 +0100

    Return the floor of the float n argument.

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 '''
-    Description: Return the floor of a float n argurment.
+    Description: Return the floor of a float n argument.
     Parameter: n: float.
 '''
 
 
 def floor(n: float) -> int:
     ''' Return the largest integer value that is <= to n. '''
-    return int(n) if n >= 0 else int(n) - 1;
+    return int(n) if n >= 0 else int(n) - 1
