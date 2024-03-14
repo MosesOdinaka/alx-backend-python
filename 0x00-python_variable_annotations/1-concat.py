0f19ed7897400116b7251cc50b772ed7d07fbfa2
root <root@DESKTOP-SK90JMN>
2024-03-14 22:05:18 +0100

    Returns concatenated string of str1 and str2 string arguments.

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+    Description: Returns concatenated string of str1 and str2 string
+                 arguments.
+    Parameter: str1: str, str2: str
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    ''' Returns the concatenation of the two string arguments. '''
+    return str1 + str2;
