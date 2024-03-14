03d2c32dbe27146271793d11420730e51c3fef72
root <root@DESKTOP-SK90JMN>
2024-03-14 22:49:13 +0100

    Return string of a float argument.

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+    Description: Return the string representation of a foat n argument.
+    Parameter: n: float.
+'''
+
+
+def to_str(n: float) -> str:
+    ''' Return string of n. '''
+    return str(n)
