7a7afbafd20f01b525cf9114494e65a47701b776
root <root@DESKTOP-SK90JMN>
2024-03-14 21:57:23 +0100

    Returns float of the sum of two float arguments.

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''
+    Description: Takes two floats as argument and return a float.
+    Parameter: a: float, b: float.
+'''
+
+def add(a: float, b: float) -> float:
+    ''' Returns the sum of a and b. '''
+    return a + b;
