a2f49725b32cb77e47803c14774ed0a799de83a3
root <root@DESKTOP-SK90JMN>
2024-03-19 13:12:35 +0100

    A async coroutine generator.

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,10 +4,11 @@ A coroutine called async_generator thats takes no arguments.
 '''
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     ''' Yield a random number between 0 and 10.'''
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
